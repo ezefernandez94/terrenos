91cd65ac9d8b33c9fdb6f2fa3de72e610f04e505
ezequiel <ezequielfernandez94@hotmail.com>
2025-07-09 18:45:41 -0300

    Se modifica el archivo de settings por una configuracion adicional de la BBDD

--- terrenos/terrenos/settings.py
+++ terrenos/terrenos/settings.py
@@ -107,7 +107,7 @@ WSGI_APPLICATION = "terrenos.wsgi.application"
 ## }
 
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("NEON_DB_URL"), conn_max_age=600, conn_health_checks=True)
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"), conn_max_age=600, conn_health_checks=True)
 }
 
 # Password validation
