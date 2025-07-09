de58353266f9462a06a90c296df6465bcaefb748
ezequiel <ezequielfernandez94@hotmail.com>
2025-07-09 18:33:01 -0300

    Se agregan configuraciones adicionales al archivo de deploy

--- build_files.sh
+++ build_files.sh
@@ -3,6 +3,9 @@
 echo "Building project packages ..."
 python3 -m pip install -r terrenos/requirements.txt
 
+echo "Creating Staticfiles directory ..."
+mkdir -p staticfiles_build/static
+
 echo "Migrating Database ..."
 python3 terrenos/manage.py makemigrations --noinput
 python3 terrenos/manage.py migrate --noinput
