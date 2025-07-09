9da24fad1ffe60bd2413a5bbec33f3dffdcb1ea3
ezequiel <ezequielfernandez94@hotmail.com>
2025-07-09 18:22:24 -0300

    Se modifica el archivo de settings por una configuracion adicional de las staticfiles

--- terrenos/terrenos/settings.py
+++ terrenos/terrenos/settings.py
@@ -144,9 +144,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
+STATIC_URL = "staticfiles/"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
