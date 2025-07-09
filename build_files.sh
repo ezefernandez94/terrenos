792359e0fe3da9ff5708595ee72d74251d06f449
ezequiel <ezequielfernandez94@hotmail.com>
2025-07-09 18:03:56 -0300

    Se modifica el archivo de build_files por errores en el deploy

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,11 @@
-# build_files.sh
+#!/usr/bin/env bash
 
-pip install -r requirements.txt
+echo "Building project packages ..."
+python3 -m pip install -r requirements.txt
+
+echo "Migrating Database ..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+echo "Collecting static files ..."
 python3 manage.py collectstatic --no-input --clear
