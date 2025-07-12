ca735b213e8f67a075773246f549ef0eb7358060
ezequiel <ezequielfernandez94@hotmail.com>
2025-07-11 22:02:31 -0300

    Se cambian configuraciones para deploy en Heroku

--- terrenos/terrenos/settings.py
+++ terrenos/terrenos/settings.py
@@ -33,7 +33,7 @@ DEBUG = os.environ.get("DJANGO_TERRENOS_DEBUG")
 
 ALLOWED_HOSTS = ['terrenos-429b9d4cf9d7.herokuapp.com/', 'localhost']
 
-CSFR_TRUSTED_ORIGINS = [
+CSRF_TRUSTED_ORIGINS = [
     'https://terrenos-429b9d4cf9d7.herokuapp.com',
     'http://localhost:8000'
 ]
